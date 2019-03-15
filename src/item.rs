15467919674c9133d9a4289a282c65df542e0371
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2019-03-15 15:24:05 -0400

    documentation for inventory and items.

--- src/item.rs
+++ src/item.rs
@@ -1,47 +1,84 @@
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 
+/// An `ItemDefinition` stores the different properties of a type of item.
+/// It is a schema that contains the data which isn't changing between different item instances.
+///
+/// Generic Types:
+/// * K: Type of the key. Usually an enum or a number (ie u32).
+/// * T: Type of the different item types. For example, an armor-type item can not be placed into a
+/// weapon-type inventory slot. If you don't need to make the distinction between different item
+/// types, use the `()` type.
+/// * D: The type of the custom user data. If you don't have any, use the `()` type. It can (and
+/// probably should) be different than the custom user data used on `ItemInstance`s
 #[derive(new, Clone, Serialize, Deserialize, Debug, Builder)]
 pub struct ItemDefinition<K, T, D: Default> {
+    /// The key identifying this item definition.
     pub key: K,
+    /// The type / item group that this item definition is part of.
     pub item_type: T,
+    /// The display name of this item definition.
     pub name: String,
+    /// The friendly name of this item definition. Mostly used to find items by name instead of by
+    /// key.
     pub friendly_name: String,
+    /// The display description of this item definition.
     pub description: String,
+    /// The maximum number of elements that can be in an item stack. Setting this value to 1
+    /// disables the ability to stack this type of item.
     pub maximum_stack: usize,
+    /// The default maximum durability of this item. Setting this to None means that this item type
+    /// doesn't use the concept of durability and is unbreakable.
     pub maximum_durability: Option<usize>,
+    /// Custom user data. For example: rarity, weight, list of allowed upgrades, etc...
     #[new(default)]
     #[builder(default)]
     pub user_data: D,
 }
 
+/// An `ItemInstance` is a stack of item.
+/// It refers to the `ItemDefinition`'s key. The associated `ItemDefinition` contains the data
+/// describing that object type.
+/// An item stack can be composed of one or many of the same item.
+/// It can also have a durability, which decreases when the item is used using
+/// `Inventory::use_item`.
+///
+/// Custom data can be added into the user_data field. This data is specific to each item instance.
+///
+/// Generic Types:
+/// * K: Type of the key. Usually an enum or a number (ie u32).
+/// * U: The type of the custom user data. If you don't have any, use the `()` type. 
+/// It can (and probably should) be different than the custom user data used on `ItemInstance`s
 #[derive(new, Clone, Serialize, Deserialize, Debug, Builder)]
 pub struct ItemInstance<K, U: Default> {
+    /// The key specifies which `ItemDefinition` defines the properties of this item stack.
     pub key: K,
+    /// The number of items in the stack.
+    /// Should not be over the associated `ItemDefinition::maximum_stack`.
     pub quantity: usize,
+    /// The remaining durability of this item, if any. If set to None, it means that the item is
+    /// unbreakable.
     #[new(default)]
     #[builder(default)]
     pub durability: Option<usize>,
+    /// The custom user data.
     #[new(default)]
     #[builder(default)]
     pub user_data: U,
 }
 
+/// A simple repository mapping the key K to the corresponding `ItemDefinition`.
 pub type ItemDefinitionRepository<K, T, D> = HashMap<K, ItemDefinition<K, T, D>>;
 
-pub trait SlotType {
-    fn can_insert_into(&self, item_type: &Self) -> bool;
+/// A trait defining which items can be inserted into each inventory slot type.
+pub trait SlotType<T> {
+    fn can_insert_into(&self, item_type: &T) -> bool;
 }
 
-impl SlotType for () {
-    fn can_insert_into(&self, _: &()) -> bool {
+impl<T> SlotType<T> for () {
+    fn can_insert_into(&self, _: &T) -> bool {
         true
-    }
-}
-
-impl SlotType for usize {
-    fn can_insert_into(&self, item_type: &usize) -> bool {
-        *self == *item_type
     }
 }
 
@@ -70,22 +107,33 @@ pub enum MoveToFrontMode {
 // the checks there.
 
 // TODO Complete slot restriction integration
+// TODO Respect maximum stack size
 
 #[derive(new, Clone, Serialize, Deserialize, Debug, Builder)]
-pub struct Inventory<K, S: SlotType, U: Default> {
+pub struct Inventory<K, T, S: SlotType<T>, U: Default> {
+    /// The contents of the `Inventory`.
+    /// None values indicate empty but existing inventory slots.
     pub content: Vec<Option<ItemInstance<K, U>>>,
     /// Restricts what kind of item can go in different slots.
     /// This is not compatible with `InventorySizingMode::Dynamic`.
+    ///
+    /// Maps to the inventory content using the index.
+    /// None values indicate that there are no restrictions for that slot.
     #[builder(default)]
     pub slot_restriction: Vec<Option<S>>,
     /// Configures how item deletion is handled.
     pub move_to_front: MoveToFrontMode,
     /// Configures if the inventory resizes when item are inserted/removed or not.
     pub sizing_mode: InventorySizingMode,
+    #[new(default)]
+    #[builder(default)]
+    _phantom: PhantomData<T>,
 }
 
-impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inventory<K, S, U> {
-    pub fn new_fixed(count: usize) -> Inventory<K, S, U> {
+impl<K: PartialEq + Clone + Debug, T, S: SlotType<T>, U: Default + Clone + Debug> Inventory<K, T, S, U> {
+
+    /// Creates a new `Inventory` with a fixed slot count.
+    pub fn new_fixed(count: usize) -> Inventory<K, T, S, U> {
         let mut content = Vec::with_capacity(count);
         (0..count).for_each(|_| content.push(None));
         let mut slot_restriction = Vec::with_capacity(count);
@@ -95,10 +143,13 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
             slot_restriction,
             move_to_front: MoveToFrontMode::None,
             sizing_mode: InventorySizingMode::new_fixed(count),
+            _phantom: PhantomData,
         }
     }
 
-    pub fn new_dynamic(minimum: usize, maximum: usize) -> Inventory<K, S, U> {
+    /// Creates a new dynamically sized `Inventory`. A minimum of `minimum` slots are garanteed to
+    /// be present at all time. The quantity of slots will not go over `maximum`.
+    pub fn new_dynamic(minimum: usize, maximum: usize) -> Inventory<K, T, S, U> {
         let mut content = Vec::with_capacity(minimum);
         (0..minimum).for_each(|_| content.push(None));
         Inventory {
@@ -106,6 +157,7 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
             slot_restriction: vec![],
             move_to_front: MoveToFrontMode::None,
             sizing_mode: InventorySizingMode::new_dynamic(minimum, maximum),
+            _phantom: PhantomData,
         }
     }
 
@@ -160,10 +212,17 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
 
     // TODO transfer no target (ie transfer all)
 
+    /// Transfers a specified quantity of item from one slot of this inventory to a specified slot
+    /// of the provided target inventory.
+    /// with_overflow indicates if the item can be spread out in free slots in case that the target
+    /// slot does not have enough free space.
+    ///
+    /// Errors:
+    /// See `Transform::delete` and `Transform::insert_into`.
     pub fn transfer(
         &mut self,
         from_idx: usize,
-        target: &mut Inventory<K, S, U>,
+        target: &mut Inventory<K, T, S, U>,
         to_idx: usize,
         quantity: usize,
         with_overflow: bool,
@@ -174,10 +233,17 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         Ok(())
     }
 
+    /// Transfers a whole stack from the specified slot into a specified slot of the provided
+    /// target directory.
+    /// with_overflow indicates if the item can be spread out in free slots in case that the target
+    /// slot does not have enough free space.
+    /// 
+    /// Errors:
+    /// See `Transform::delete` and `Transform::insert_into`.
     pub fn transfer_stack(
         &mut self,
         from_idx: usize,
-        target: &mut Inventory<K, S, U>,
+        target: &mut Inventory<K, T, S, U>,
         to_idx: usize,
         with_overflow: bool,
     ) -> Result<(), ItemError<K, U>> {
@@ -188,6 +254,12 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         }
     }
 
+    /// Moves a specified quantity of item from a slot to another.
+    /// with_overflow indicates if the item can be spread out in free slots in case that the target
+    /// slot does not have enough free space.
+    ///
+    /// Errors:
+    /// See `Inventory::delete` and `Inventory::insert_into`.
     pub fn move_item(
         &mut self,
         from_idx: usize,
@@ -201,7 +273,13 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
     }
 
     // TODO: swap item stacks
-
+    
+    /// Moves a full stack of item from a slot to another.
+    /// with_overflow indicates if the item can be spread out in free slots in case that the target
+    /// slot does not have enough free space.
+    ///
+    /// Errors:
+    /// * SlotEmpty: Nothing is present in the specified slot.
     pub fn move_stack(
         &mut self,
         from_idx: usize,
@@ -215,6 +293,11 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         }
     }
 
+    /// Deletes a specified quantity of item from the specified slot.
+    /// 
+    /// Errors:
+    /// * NotEnoughQuantity: Not enough items are present in the item stack.
+    /// * SlotEmpty: Nothing is present in the specified slot.
     pub fn delete(
         &mut self,
         idx: usize,
@@ -263,6 +346,10 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         }
     }
 
+    /// Deletes a full stack of item at the provided index and returns it.
+    ///
+    /// Errors:
+    /// See `Transform::delete`.
     pub fn delete_stack(&mut self, idx: usize) -> Result<ItemInstance<K, U>, ItemError<K, U>> {
         if let Some(Some(ii)) = self.content.get(idx) {
             self.delete(idx, ii.quantity)
@@ -271,6 +358,11 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         }
     }
 
+    /// Deletes items by matching the key until the deleted quantity reaches the specified
+    /// quantity.
+    ///
+    /// Errors:
+    /// * NotEnoughQuantity: Not enough items with the specified key are present in the inventory.
     pub fn delete_key(
         &mut self,
         key: &K,
@@ -307,6 +399,7 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         unreachable!();
     }
 
+    /// Checks if the total quantity of items of the specified key are present in the inventory.
     pub fn has_quantity(&self, key: &K, quantity: usize) -> bool {
         let sum: usize = self
             .content
@@ -358,6 +451,12 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
             .filter(move |ii| ii.key == key)
     }
 
+    /// Inserts the `ItemInstance` into the specified index.
+    ///
+    /// It will eventually attempt to merge stacks together, but this is not implemented yet.
+    ///
+    /// Errors:
+    /// * SlotOccupied: The slot is currently occupied by another item type.
     pub fn insert_into(
         &mut self,
         idx: usize,
@@ -375,9 +474,17 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         }
     }
 
+    /// Inserts the `ItemInstance` at the first available inventory space.
+    /// If the inventory is dynamically size, it will attempt to create a slot and insert into it.
+    ///
+    /// It will eventually attempt to merge stacks together, but this is not implemented yet.
+    ///
+    /// Errors:
+    /// * InventoryFull: The inventory is full and no more space can be created.
     pub fn insert(&mut self, item: ItemInstance<K, U>) -> Result<(), ItemError<K, U>> {
         if let Some(slot) = self.first_empty_slot() {
-            self.insert_into(slot, item)
+            self.insert_into(slot, item).unwrap();
+            Ok(())
         } else {
             match self.sizing_mode {
                 InventorySizingMode::Fixed { size: _ } => Err(ItemError::InventoryFull),
@@ -388,7 +495,8 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
                     // Attempt to make room.
                     if self.has_space() {
                         self.content.push(None);
-                        self.insert_into(self.content.len() - 1, item)
+                        self.insert_into(self.content.len() - 1, item).unwrap();
+                        Ok(())
                     } else {
                         Err(ItemError::InventoryFull)
                     }
@@ -397,6 +505,7 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
         }
     }
 
+    /// Returns the first empty slot if any is available.
     pub fn first_empty_slot(&self) -> Option<usize> {
         match self.move_to_front {
             MoveToFrontMode::None => {
@@ -439,18 +548,31 @@ impl<K: PartialEq + Clone + Debug, S: SlotType, U: Default + Clone + Debug> Inve
     //pub fn first_empty_slot_filtered(&self,
 }
 
+/// The different errors that can happen when interacting with the `Inventory`.
 #[derive(Debug)]
 pub enum ItemError<K: PartialEq + Debug, U: Default> {
+    /// The stack doesn't fit completely inside of the slot.
     StackOverflow(ItemInstance<K, U>),
+    /// The inventory is full and cannot be resized anymore.
     InventoryFull,
+    /// The inventory cannot fit the specified items inside of itself.
+    /// It has no more empty slots, cannot be resized and no item can be stacked with others.
     InventoryOverflow(Vec<ItemInstance<K, U>>),
+    /// The item was used and the durability is now 0.
     ItemDestroyed(ItemInstance<K, U>),
+    /// The stack size was decreased and is now 0.
     StackConsumed(ItemInstance<K, U>),
+    /// The slot already has something inside of it.
     SlotOccupied,
+    /// The specified item cannot be inserted into this type of slot.
     SlotRestricted,
+    /// The origin slot is locked. The item cannot be moved or inserted.
     LockedOriginSlot,
-    LocketRemoteSlot,
+    /// The remote slot is locked. The item cannot be moved or inserted.
+    LockedRemoteSlot,
+    /// The slot at the specified index is empty or non-existant.
     SlotEmpty,
+    /// There is not enough of the specified item to satisfy the query.
     NotEnoughQuantity,
 }
 
@@ -490,29 +612,39 @@ mod test {
     }
 
     #[derive(new, Debug, Clone, Serialize, Deserialize, PartialEq)]
-    enum ItemType {
+    enum CustomSlotType {
         Regular,
+        Equipment,
+    }
+
+    #[derive(new, Debug, Clone, Serialize, Deserialize, PartialEq)]
+    enum ItemType {
+        Other,
+        Weapon,
         Armor,
-        Heavy,
+        Consumable,
     }
 
     // Work around for broken derive_builder.
     impl Default for ItemType {
         fn default() -> Self {
-            ItemType::Regular
+            ItemType::Other
         }
     }
 
-    impl SlotType for ItemType {
+    impl SlotType<ItemType> for CustomSlotType {
         fn can_insert_into(&self, other: &ItemType) -> bool {
-            *self == *other
+            match *self {
+                CustomSlotType::Regular => true,
+                CustomSlotType::Equipment => *other == ItemType::Weapon || *other == ItemType::Armor,
+            }
         }
     }
 
     #[test]
     fn inventory_insert_empty_fixed() {
         let ii = ItemInstance::<u32, ()>::new(1u32, 1);
-        let mut inv = Inventory::<u32, (), ()>::new_fixed(8);
+        let mut inv = Inventory::<u32, (), (), ()>::new_fixed(8);
         inv.insert(ii).expect("");
     }
 
@@ -523,7 +655,7 @@ mod test {
 
         let item_def = ItemDefinitionBuilder::default()
             .key(1u32)
-            .item_type(ItemType::Regular)
+            .item_type(ItemType::Consumable)
             .name("Apple".to_string())
             .friendly_name("apple".to_string())
             .description("Food is nice".to_string())
@@ -539,7 +671,7 @@ mod test {
             .user_data(CustomItemInstanceData::new(0.0))
             .build()
             .unwrap();
-        let mut inv = Inventory::<u32, ItemType, CustomItemInstanceData>::new_fixed(8);
+        let mut inv = Inventory::<u32, ItemType, CustomSlotType, CustomItemInstanceData>::new_fixed(8);
         inv.insert(ii).expect("");
     }
 }
